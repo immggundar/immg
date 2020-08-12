8388339ce1449f17fbe1191b5e6f1138efd3d61b
immggundar <69563109+immggundar@users.noreply.github.com>
2020-08-12 13:42:09 +0700

    Update models.py

--- peoplemanagement/user/models.py
+++ peoplemanagement/user/models.py
@@ -33,8 +33,8 @@ class Profile(models.Model):
     birth_date = models.DateField(default=timezone.now)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
